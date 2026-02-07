c6a8d5efef8d8552f09f3b10ec75b6356a4df18d
Mohan Kumar <moha123live@gmail.com>
2026-02-07 22:54:56 +0530

    cluster added

---
+++ cluster.js
@@ -0,0 +1,20 @@
+const cluster = require("cluster");
+const os = require("os");
+
+const numCPUs = os.cpus().length;
+
+if (cluster.isPrimary) {
+    console.log(`Master ${process.pid} is running with Forking ${numCPUs} workers`);
+
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on("exit", (worker, code, signal) => {
+        console.error(`Worker ${worker.process.pid} died. Restarting...`);
+        cluster.fork();
+    });
+
+} else {
+    require("./index.js");
+}
--- index.js
+++ index.js
@@ -2,54 +2,92 @@ require("./config/env");
 require("./src/v1/utils/constants");
 const express = require("express");
 const cors = require("cors");
-const { connectDB } = require("./config/database");
-const syncModels = require("./src/v1/models/dbSync");
+const { sequelize, connectDB } = require("./config/database");
+// const syncModels = require("./src/v1/models/dbSync");
 const errorHandler = require("./src/v1/middlewares/errorHandler");
 const routes = require("./src/v1/routes");
 
 const app = express();
 const FRONTEND_URL = process.env.FRONT_END;
-app.use(cors({
-    origin: FRONTEND_URL || "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
-}));
 
-app.use(express.json({ limit: '2mb' }));
-app.use(express.urlencoded({ extended: true, limit: '2mb' }));
+app.use(
+    cors({
+        origin: FRONTEND_URL || "*",
+        methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
+    })
+);
+
+app.use(express.json({ limit: "2mb" }));
+app.use(express.urlencoded({ extended: true, limit: "2mb" }));
 
 app.use("/api/v1", routes);
-app.get('/api/ping', (req, res) => {
-    res.status(200).json({ 
-        message: 'API is running smoothly!',
-        database: 'PostgreSQL'
+
+
+app.get("/api/worker", (req, res) => {
+  res.status(200).json({
+    workerPid: process.pid,
+    message: 'working fine'
+  });
+});
+
+app.get("/api/ping", (req, res) => {
+    res.status(200).json({
+        message: "API is running smoothly!",
+        database: "PostgreSQL",
     });
 });
 
-app.get('/api/health', (req, res) => {
-    res.status(200).json({ 
-        status: 'healthy',
-        timestamp: new Date().toISOString()
+app.get("/api/health", (req, res) => {
+    res.status(200).json({
+        status: "healthy",
+        timestamp: new Date().toISOString(),
     });
 });
+
+
+
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 
+let server;
+
 const startServer = async () => {
     try {
         await connectDB();
-        console.log('Database connected');
-        await syncModels();
-        app.listen(PORT, '0.0.0.0', () => {
-            console.log(`Environment: ${process.env.NODE_ENV || 'development'} - Server running on port ${PORT}`);
+        // console.log(`Worker ${process.pid} - Database connected`);
+        server = app.listen(PORT, "0.0.0.0", () => {
+            console.log(`Worker ${process.pid} - Environment: ${process.env.NODE_ENV || "development"
+                } - Server running on port ${PORT}`);
         });
     } catch (error) {
-        console.error("Server startup error:", error.message);
-        console.error("Stack trace:", error.stack);
+        console.error("Server error:", error);
         process.exit(1);
     }
 };
 
-startServer();
+startServer(); 
+
+
+const shutdown = async (signal) => {
+  console.log(`\nWorker ${process.pid} received ${signal}. Shutting down gracefully...`);
+  if (server) {
+    console.log('coming inside');
+    
+    server.close(async () => {
+      try {
+        await sequelize.close();
+        console.log('sequlizer closed')
+        process.exit(0);
+      } catch (err) {
+        console.error("Error closing DB connection", err);
+        process.exit(1);
+      }
+    });
+  }
+};
+
+process.on("SIGTERM", shutdown);
+process.on("SIGINT", shutdown);
